2f13e9ca9591f693a0039063f1a31cc1b6ad9132
dimlau <dimlau@gmail.com>
2021-11-15 15:50:33 +0800

    扩大搜索范围

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -60,7 +60,6 @@ class StatusesIndex < Chewy::Index
         field :stemmed, type: 'text', analyzer: 'content'
       end
 
-      field :searchable_by, type: 'long', value: ->(status, crutches) { status.searchable_by(crutches) }
     end
   end
 end
