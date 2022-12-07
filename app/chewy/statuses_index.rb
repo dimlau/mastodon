aa68ff81bfeba2ff00ee2946aa0bb0650f1b3906
dimlau <dimlau@gmail.com>
2022-12-07 14:57:10 +0800

    elastic 插件

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -18,14 +18,6 @@ class StatusesIndex < Chewy::Index
         language: 'possessive_english',
       },
     },
-    char_filter: {
-      tsconvert: {
-        type: 'stconvert',
-        keep_both: false,
-        delimiter: '#',
-        convert_type: 't2s',
-      },
-    },
     analyzer: {
       content: {
         tokenizer: 'ik_max_word',
@@ -37,7 +29,6 @@ class StatusesIndex < Chewy::Index
           english_stop
           english_stemmer
         ),
-        char_filter: %w(tsconvert),
       },
     },
   }
