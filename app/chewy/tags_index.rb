f0512677dff2b12b08405a363e337d5e23a7674f
dimlau <dimlau@gmail.com>
2022-12-26 17:55:58 +0800

    elastic 插件++

--- app/chewy/tags_index.rb
+++ app/chewy/tags_index.rb
@@ -2,6 +2,14 @@
 
 class TagsIndex < Chewy::Index
   settings index: { refresh_interval: '30s' }, analysis: {
+    char_filter: {
+      tsconvert: {
+        type: 'stconvert',
+        keep_both: false,
+        delimiter: '#',
+        convert_type: 't2s',
+      },
+    },
     analyzer: {
       content: {
         tokenizer: 'ik_max_word',
